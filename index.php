57a2752796007d801f8c5a11982be7972b9cbf46
Andy Ball <932400399@bun2-206-11.local>
2019-10-07 11:51:38 +1300

    notes and content for variable types

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 	// $phpinfo 		// there are no reserved words in PHP!
 	$title = 'Introduction to PHP';
 
-	$string 	= `this is a string of text`;
+	$string 	= '"this is a string of text"';
 	$integer 	= 3;		// whole numbers only
 	$float 		= 3.254;	// anything that's not a whole number
 	$boolean 	= false;
@@ -36,7 +36,7 @@
 		}
 
 		$myself = new Me();
-		echo $myself->name;
+		// echo $myself->name;
 
 		// null
 		// null != nothing
@@ -45,7 +45,18 @@
 		// constants
 		define('PAGE_HEADER', 'Hello, world! This message was defined in a constant variable');	// define(); takes two values: an element's name, and the value we want to define
 
+	// var dumping
+	echo '<pre>';
+	var_dump($array);
+	echo '</pre>';
 
+	// printing
+	echo '<pre>';
+	print_r($array);
+	echo '</pre>';
+
+	// die();
+	die($string);	// this line prints out $string, then stops the script. Kind of like 'pause' in Chrome
 ?>
 
 <!DOCTYPE html>
@@ -60,9 +71,18 @@
 			// Parse error: syntax error, unexpected 'echo' (T_ECHO), expecting ',' or ';' in /var/www/html/phpIntro/index.php on line 31
 			// often you're looking for an error on the line above the one that threw the error
 
-			echo '<h1>Hello, world, and welcome to our PHP adventure!</h1>';
-			echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
 
+			echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
 		?>
+
+		<h1> <?php echo PAGE_HEADER; ?> </h1>
+		<p>Our string variable is: <?php echo $string; ?></p>
+		<p>Our integer variable is: <?php echo $integer; ?></p>
+		<p>Our float variable is: <?php echo $float; ?></p>
+		<p>Our Boolean variable is: <?php echo $boolean; ?></p>
+		<p>Our null variable is: <?php echo $null; ?></p>
+		<p>Our boolean variable is: <?php echo $boolean; ?></p>
+		<p>Our object's 'name' value is: <?php echo $myself->name; ?></p>
+		<p>A value in our array is: <?php echo $array[0]; ?></p>
 	</body>
 </html>
